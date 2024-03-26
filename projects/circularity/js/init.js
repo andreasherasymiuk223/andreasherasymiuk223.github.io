24305d11575cc7f7d3747b78416177624014c9a8
Andreas Herasymiuk <andy.herasymiuk@gmail.com>
2024-03-26 16:42:30 +0000

    added runtime

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+        var circle;
+        var circles = [];
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        
+        for (var x = 0; x < 100; x++) {
+            drawCircle()
+          }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -37,15 +45,25 @@ var init = function (window) {
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-        function update() {
+        function update() { 
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(circles[0])
+            physikz.updatePosition(circles[1])
+            physikz.updatePosition(circles[2])
+            physikz.updatePosition(circles[3])
+            physikz.updatePosition(circles[4])
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+            game.checkCirclePosition(circles[0])
+            game.checkCirclePosition(circles[1])
+            game.checkCirclePosition(circles[2])
+            game.checkCirclePosition(circles[3])
+            game.checkCirclePosition(circles[4])
             // TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++) {
+                i()
+              }
+              
             
         }
     
@@ -60,10 +78,17 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            
+        
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+            if ( circle.y > canvas.height) {
+                circle.y = 0;
+            } 
+            if ( circle.y < 0) {
+                circle.y = canvas.height;
+            }
+            if ( circle.x < 0) {
+                circle.x = canvas.width;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
